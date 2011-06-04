b70d2bb42c0c5c6f658a0a6ec418b2ec996979e4
Shanon McQuay <shanonmcquay@gmail.com>
2011-06-05 01:34:55 +1000

    006 Optimized - Replaced brute force calculations with mathematical equivalents.

--- 006.rb
+++ 006.rb
@@ -1,1 +1,6 @@
-p (1..100).reduce(:+) ** 2 - (1..100).inject(0) { |total, number| total + number ** 2 }
+max = 10000000
+
+square_sum = (max * (max + 1) / 2) ** 2
+sum_of_squares = (2 * max + 1) * (max + 1) * max / 6
+
+p square_sum - sum_of_squares
