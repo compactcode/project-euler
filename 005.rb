a9c1e989a7353c4dc4587b16e2e919f582611d56
Shanon McQuay <shanonmcquay@gmail.com>
2011-06-04 19:01:59 +1000

    005 Solved - Brute force

--- 005.rb
+++ 005.rb
@@ -1,7 +1,15 @@
-match = 0
-current = 20
-while match == 0
-  match = current unless (2..10).map { |x| current.modulo(x) == 0 }.include?(false)
-  current += 1
+candidates = []
+
+20.downto(1).each do |x|
+  candidates << x unless candidates.count { |candidate| candidate % x == 0 } > 0
 end
+
+match = 0
+current = 0
+while match == 0
+  match = current if candidates.take_while { |x| current.modulo(x) == 0 }.size == candidates.size
+  current += 1
+  p current if current % 100000 == 0
+end
+
 p match
