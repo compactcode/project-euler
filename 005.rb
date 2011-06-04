01e6063a96db71dfe28b7d8b1f4cd7b686efae59
Shanon McQuay <shanonmcquay@gmail.com>
2011-06-04 23:46:15 +1000

    005 Cleaned - Removed old prime factorization code.

--- 005.rb
+++ 005.rb
@@ -1,11 +1,1 @@
-require 'prime'
-
-factors = Hash.new(0)
-
-(2..20).map { |x| Prime.prime_division(x) }.flatten(1).each { |prime_factors|
-  factors[prime_factors.first] = [prime_factors.last, factors[prime_factors.first]].max
-}
-
-p factors.map { |factor, occurrances| factor ** occurrances }.reduce(:*)
-
 p (1..20).inject(1) { |result, n| result.lcm n }
