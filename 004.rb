876f01689a787396e315b30532e32c73dbec61b7
Shanon McQuay <shanonmcquay@gmail.com>
2011-06-04 16:21:33 +1000

    002 Optimized - Reduced calculations by 50%

--- 004.rb
+++ 004.rb
@@ -1,6 +1,6 @@
 largest = 0
 100.upto(999) do |first_factor|
-  100.upto(999) do |second_factor|
+  first_factor.upto(999) do |second_factor|
     product = first_factor * second_factor
     largest = product if product > largest && product.to_s == product.to_s.reverse
   end
