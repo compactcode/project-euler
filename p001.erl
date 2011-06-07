5492dd6925d5e0b52fbe99458d130dda8d66267c
Shanon McQuay <shanonmcquay@gmail.com>
2011-06-07 20:04:27 +1000

    001 Cleaned(Erlang) - Removed imports

--- p001.erl
+++ p001.erl
@@ -1,7 +1,6 @@
 -module(p001).
--import(lists, [seq/3, sum/1]).
 -export([answer/0]).
 
 answer() -> sum_multiples(3) + sum_multiples(5) - sum_multiples(15). 
   
-sum_multiples(Of) -> sum(seq(0, 999, Of)).
+sum_multiples(Of) -> lists:sum(lists:seq(0, 999, Of)).
