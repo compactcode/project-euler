58e7b6fb09aabe28cdbff7130278ba8e50f0763b
Shanon McQuay <shanonmcquay@gmail.com>
2011-06-08 16:50:35 +1000

    002 Optimized(Erlang) - Use arithmatic to ignore odd numbers.

--- p002.erl
+++ p002.erl
@@ -3,9 +3,5 @@
 
 answer() -> fib(0, 1, 2, 4000000).
 
-fib(Sum, L, R, Max) when R < Max ->
-  case R rem 2 == 0 of
-    true  -> fib(Sum + R, R, L + R, Max);
-    false -> fib(Sum, R, L + R, Max)
-  end;
+fib(Sum, L, R, Max) when R < Max -> fib(Sum + (R - R rem 2 * R), R, L + R, Max);
 fib(Sum, _, _, _) -> Sum.
