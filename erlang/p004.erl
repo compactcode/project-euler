dd32753d867bd2dcf08ef5465d0f7105b5475768
Shanon McQuay <shanonmcquay@gmail.com>
2011-06-11 21:55:24 +1000

    004 Optimized(Erlang) - Avoid creating stacks of lists

--- erlang/p004.erl
+++ erlang/p004.erl
@@ -1,14 +1,15 @@
 -module(p004).
--export([answer/0, combinations/1, largest_palindrome/1]).
+-export([answer/0]).
 
-answer() -> largest_palindrome(combinations(lists:seq(100, 999))).
+answer() -> solve(lists:seq(100, 999)).
 
-combinations(List) -> combinations(List, []).
-combinations([], Out) -> Out;
-combinations([H | T], Out) -> combinations(T, Out ++ [X * H || X <- T]).
+solve(Seq) -> solve(Seq, lists:max(Seq), 0).
+solve([], _, Largest) -> Largest;
+solve([H | T], Max, Largest) -> 
+  Palindromes = [palindrome(X * H) || X <- lists:seq(H, Max)],
+  solve(T, Max, max(Largest, lists:max(Palindromes))).
 
-largest_palindrome(List) -> 
-  lists:max(lists:filter(fun(N) -> is_palindrome(N) end, List)).
-  
-is_palindrome(N) when is_integer(N) -> is_palindrome(erlang:integer_to_list(N));
-is_palindrome(N) -> N == lists:reverse(N).
+palindrome(N) when is_integer(N) -> palindrome(integer_to_list(N));
+palindrome(N)                    -> palindrome(N, N == lists:reverse(N)).
+palindrome(_, false)             -> 0;
+palindrome(N, true)              -> list_to_integer(N).  
