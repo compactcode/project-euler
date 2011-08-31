ac3eb370d075e29fd4a0e5c2b8febfc0f052e353
Shanon McQuay <shanonmcquay@gmail.com>
2011-08-31 23:25:43 +1000

    009 Optimized(Ruby) - Use substitution instead of brute force.

--- ruby/009.rb
+++ ruby/009.rb
@@ -1,12 +1,4 @@
-# A Pythagorean triplet is a set of three natural numbers, a  b  c, for which,
-#
-# a^2 + b^2 = c^2
-# For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
-#
-# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-# Find the product abc.
-
-max = 1000 / 2
+max = 1000
 
 def is_triplet?(a, b, c)
   a ** 2 + b ** 2 == c ** 2
@@ -14,8 +6,7 @@ end
 
 3.upto(max) do |a|
   a.upto(max) do |b|
-    b.upto(max) do |c|
-      p a * b * c if is_triplet?(a, b, c) && (a + b + c == 1000)
-    end
+    c = max - a - b
+    p a * b * c if is_triplet?(a, b, c)
   end
 end
